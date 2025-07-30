1d74d52e78c970f4065b9895737e96d547ccbbe5
Seddik Sahraoui <seddiksah123@gmail.com>
2025-07-30 19:03:26 -0400

    Added description on the new columns

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ with col2:
     Hi, I am Seddik! I am a Python Programmer.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
